d49a4d1ffb3f7da4abbb980e642777ed7e5cd349
Carl in 't Veld <carl@intveld.nl>
2019-10-15 20:38:57 +0200

    Modal with mat blazor (#3)

--- BlazorApp1/Blazor Server-Side/Program.cs
+++ BlazorApp1/Blazor Server-Side/Program.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using BlazorServerSide.ConnectedAppServiceInstances;
 using BlazorServerSide.Data;
 
-namespace BlazorApp1
+namespace BlazorServerSide
 {
     public class Program
     {
